cc0c8129a45d538a697843639dc1ae017447e802
Sir Vimbi <kiptoo.bryan@gmail.com>
2026-08-14 14:20:21 +0300

    Provide cross-platform MT5 disconnect control type

---
+++ ForexScalperApp/MT5DisconnectButtonNonMac.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+#if !os(macOS)
+private struct MT5DisconnectButton: View {
+    @ObservedObject var viewModel: DashboardViewModel
+    var body: some View {
+        Button {
+            Task { await viewModel.disconnectFromMT5() }
+        } label: {
+            Label("DISCONNECT MT5", systemImage: "power")
+                .font(.caption.bold())
+                .foregroundColor(.red)
+        }
+        .disabled(!viewModel.mt5Connected)
+    }
+}
+#endif
